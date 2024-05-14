72d8cb8dd87781f0ff1d39cfafbd0fa9b9f9493c
lordran <damianomarletta25@gmail.com>
2024-05-14 16:46:21 +0200

    aggiornamenti article

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
 
 Route::get('/', [PublicController::class , 'homepage'])->name('homepage');
+
+Route::('/articles/create' , [ArticleController::class , 'create'])->('article.create');
